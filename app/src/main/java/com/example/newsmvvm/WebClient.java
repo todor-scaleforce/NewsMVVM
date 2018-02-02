dfcfa7bf3f4d72f3fe63b96ea6517b5a5604ae87
Unknown <todor.kostov@scaleforce.io>
2018-02-03 00:09:41 +0200

    Basic WebClient included for handling HTTP requests

---
+++ app/src/main/java/com/example/newsmvvm/WebClient.java
@@ -0,0 +1,44 @@
+package com.example.newsmvvm;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.MySSLSocketFactory;
+
+import java.security.KeyStore;
+
+/**
+ * Created by tosheto on 2/3/2018.
+ */
+
+public class WebClient {
+
+    private static final String TAG = WebClient.class.getSimpleName();
+
+    private static WebClient client = null;
+
+    private AsyncHttpClient asyncHttpClient;
+
+
+    private WebClient() {
+        asyncHttpClient = new AsyncHttpClient();
+        asyncHttpClient.setTimeout(120000);
+
+        MySSLSocketFactory socketFactory = null;
+        try {
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+            socketFactory = new MySSLSocketFactory(trustStore);
+            socketFactory.setHostnameVerifier(MySSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            asyncHttpClient.setSSLSocketFactory(socketFactory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static WebClient getInstance() {
+        if(client == null)
+            client = new WebClient();
+
+        return client;
+    }
+}
