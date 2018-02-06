8376a7a8af02b82109e63fcebc255ecf52e8bf1e
Unknown <todor.kostov@scaleforce.io>
2018-02-07 00:11:06 +0200

    Basic UI for MainActivity included
    New string resources
    Click event handlers included for MainActivity

--- app/src/main/java/com/example/newsmvvm/MainActivity.java
+++ app/src/main/java/com/example/newsmvvm/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.newsmvvm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bTopHeadlines;
+    private Button bEverything;
+    private Button bSources;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bTopHeadlines = (Button) findViewById(R.id.top_headlines_button);
+        bEverything = (Button) findViewById(R.id.everything_button);
+        bSources = (Button) findViewById(R.id.sources_button);
+
+        // TOP HEADLINES CLICK
+        bTopHeadlines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Top Headlines", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // EVERYTHING CLICK
+        bEverything.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Everything", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // SOURCES CLICK
+        bSources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sources", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
