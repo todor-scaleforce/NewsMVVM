5467249e1b7885f2f861484dd8f1a197edb2ced2
Unknown <todor.kostov@scaleforce.io>
2018-02-14 23:59:26 +0200

    Minor UI fixes for TopHeadlinesActivity

--- app/src/main/java/com/example/newsmvvm/activities/TopHeadlinesActivity.java
+++ app/src/main/java/com/example/newsmvvm/activities/TopHeadlinesActivity.java
@@ -3,6 +3,7 @@ package com.example.newsmvvm.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,10 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.newsmvvm.R;
+import com.example.newsmvvm.adapters.ArticlesAdapter;
+import com.example.newsmvvm.models.Article;
+
+import java.util.ArrayList;
 
 /**
  * Created by tosheto on 2/7/2018.
@@ -25,6 +30,8 @@ public class TopHeadlinesActivity extends AppCompatActivity {
     private LinearLayout somethingWrongContainer;
     private Button retryButton;
 
+    private ArticlesAdapter articlesAdapter;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +42,8 @@ public class TopHeadlinesActivity extends AppCompatActivity {
         topHeadlinesList = (RecyclerView) findViewById(R.id.top_headlines_list);
         somethingWrongContainer = (LinearLayout) findViewById(R.id.something_wrong_container);
         retryButton = (Button) findViewById(R.id.retry_button);
+
+        topHeadlinesList.setLayoutManager(new LinearLayoutManager(this));
 
         // CLICK RETRY BUTTON
         retryButton.setOnClickListener(new View.OnClickListener() {
