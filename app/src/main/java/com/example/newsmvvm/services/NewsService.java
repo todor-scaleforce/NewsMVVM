014525a6bdba20326ad7b4e0f5f75893ec2567de
Unknown <todor.kostov@scaleforce.io>
2018-02-18 14:01:04 +0200

    Basic implementation of fetchTopHeadlinesForCountry included
    New Endpoints parameter included.

--- app/src/main/java/com/example/newsmvvm/services/NewsService.java
+++ app/src/main/java/com/example/newsmvvm/services/NewsService.java
@@ -3,7 +3,14 @@ package com.example.newsmvvm.services;
 import android.util.Log;
 
 import com.example.newsmvvm.WebClient;
+import com.example.newsmvvm.models.ArticlesResponse;
 import com.example.newsmvvm.models.Country;
+import com.example.newsmvvm.utils.EndpointsConstants;
+import com.loopj.android.http.BaseJsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.ResponseHandlerInterface;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by tosheto on 2/18/2018.
@@ -34,9 +41,31 @@ public class NewsService {
     }
 
 
-    public void fetchTopHeadlinesForCountry(Country country, ) {
+    public void fetchTopHeadlinesForCountry(Country country, IServiceResponse serviceResponse) {
         Log.d(TAG, "fetchTopHeadlinesForCountry: hit");
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.put(EndpointsConstants.COUNTRY_PARAMETER, country.getAbbreviation());
+
         webClient.getAsyncHttpClient()
-                .addHeader(WebClient.AUTHORIZATION_HEADER, apiKey)
+                .addHeader(WebClient.AUTHORIZATION_HEADER, apiKey);
+        webClient.getAsyncHttpClient()
+                .get(EndpointsConstants.TOP_HEADLINES, requestParams, new BaseJsonHttpResponseHandler<ArticlesResponse>() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, ArticlesResponse response) {
+                        Log.d(TAG, "onSuccess: hit");
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, ArticlesResponse errorResponse) {
+                        Log.d(TAG, "onFailure: hit");
+                    }
+
+                    @Override
+                    protected ArticlesResponse parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
+                        Log.d(TAG, "parseResponse: hit");
+                        return null;
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/newsmvvm/utils/EndpointsConstants.java
+++ app/src/main/java/com/example/newsmvvm/utils/EndpointsConstants.java
@@ -6,7 +6,11 @@ package com.example.newsmvvm.utils;
 
 public class EndpointsConstants {
 
+    // ENDPOINTS
     public static final String BASE_ENDPOINT = "https://newsapi.org/v2/";
     public static final String TOP_HEADLINES = BASE_ENDPOINT + "top-headlines";
 
+
+    // ENDPOINTS PARAMETERS
+    public static final String COUNTRY_PARAMETER = "country";
 }
